9a8ca561c251a43f8911ec49d5517fc29fed239c
Honghui Choi <chh8259@hotmail.com>
2013-09-11 12:38:32 -0500

    first commit

---
+++ SecureSystem.java
@@ -0,0 +1,56 @@
+/*
+cs361 (Bill Young)
+Assignment1 
+Honghui Choi
+hc7795
+*/
+//dkTlqkf whsskrp gkrltlfgdj. dksi dlwp akwlakrgkrrlwksgdk. duftalgl gksqjs goqhwk.
+import java.util.*;
+import java.io.IOException;
+import java.io.FileReader;
+
+public class SecureSystem {
+	//HashMap<String, Integer> subject;	
+	//HashMap<String, Integer> Lyle;
+	
+	public static void main(String[] args) throws IOException{
+		Scanner reader = new Scanner(new FileInputStream(args[0]));
+		/*
+		SecurityLevel sl = new SecurityLevel();
+		SecurityLevel low = sl.LOW;
+		SecurityLevel high = sl.HIGH;
+		
+		sys = new SecureSystem();
+		
+		sys.createSubject("Lyle", low);
+		sys.createSubject("Hal", high);
+		
+		//sys.getReferenceMonitor().createNewObject("Lobj", low);
+		//sys.getReferenceMonitor().createNewObject("Hobj", low);
+		*/
+		 
+    }
+	/*
+	public void createSubject(String subjectName, SecurityLevel level) {
+		this.subject.put(subjectName, level);
+	}
+	*/
+	//public void createNewObjecy() {
+	//}
+}
+/*
+class SecurityLevel {
+	final int LOW = 0;
+	final int HIGH = 1;
+	private int level;	
+	
+	public SecurityLevel (int level) {
+		System.out.println("hello, a constructor.");
+		this.level = level;
+	}
+	
+	int getLevel () {
+		return this.level;
+	}
+}
+*/
