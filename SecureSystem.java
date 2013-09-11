0c5ba9c5addd0c74e9314e0ad4cb1118bd50f9e2
hc7795 <hc7795@cs.utexas.edu>
2013-09-11 16:49:00 -0500

    Update SecureSystem.java

--- SecureSystem.java
+++ SecureSystem.java
@@ -4,49 +4,70 @@ Assignment1
 Honghui Choi
 hc7795
 */
+//dkTlqkf whsskrp gkrltlfgdj. dksi dlwp akwlakrgkrrlwksgdk. duftalgl gksqjs goqhwk.
 import java.util.*;
 import java.io.IOException;
 import java.io.FileReader;
 
 public class SecureSystem {
 	HashMap<String, SecurityLevel> subject;	
-
+	HashMap<String, SecurityLevel> object;
+	HashMap<String, Integer> subjectTemp;
+	HashMap<String, Integer> objectValue;
+	
 	public static void main(String[] args) throws IOException{
-		/*
 		Scanner reader = new Scanner(new FileInputStream(args[0]));
+		SecureSystem sys = new SecureSystem();
+		
 		while(reader.hasNext()) {
 			String line = reader.next();
 			String[] words = instruction.split("\\s+");
-			if(words[0].equals("write"))
+			if(words[0].equals("read")) {
 				//write method
-			else if(words[0].equals("read"))
+				sys.read(words[1], words[2]);
+				
+			}
+			else if(words[0].equals("write"))
 				//read method
 			else
 				//illegal instructions?
 			
 		}
-		*/
+		/*
 		SecurityLevel low = SecurityLevel.LOW;
 		SecurityLevel high = SecurityLevel.HIGH;
 		
-		SecureSystem sys = new SecureSystem();
 		
 		sys.createSubject("Lyle", low);
 		sys.createSubject("Hal", high);
 		
+		sys.createObject("LObj", 0);
+		sys.createObject("HObj", 0);
+		
 		//sys.getReferenceMonitor().createNewObject("Lobj", low);
 		//sys.getReferenceMonitor().createNewObject("Hobj", low);
+		*/
 
     }
-	
+	/*
 	public void createSubject(String subjectName, SecurityLevel level) {
 		this.subject.put(subjectName, level);
 	}
+	public void createObject(String objectName, SecurityLevel level) {
+		this.object.put(objectName, level);
+	}
 	
-	//public void createNewObjecy() {
+	//public void createNewObject() {
 	//}
+	*/
+	public void read(String subject, String object) {
+		if(this.subject.get(subject) > this.object.get(object)) {
+			int objVal = this.objectValue.get(object);
+			this.subjectTemp.put(subject, objVal);
+		}
+	}
 }
-
+/*
 class SecurityLevel {
 	final static SecurityLevel LOW;
 	final static SecurityLevel HIGH;
@@ -59,3 +80,4 @@ class SecurityLevel {
 		this.level = level;
 	}
 }
+*/
