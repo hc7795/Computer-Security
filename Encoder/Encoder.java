eef4ca2b1dac4a3b41079c3894faddef131e705b
Honghui Choi <chh8259@hotmail.com>
2013-10-07 12:19:58 -0500

    read in inputs, and huffman from online

---
+++ Encoder/Encoder.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Encoder{
+	
+	
+	public static void main(String args[]) throws IOException{
+		File input = new File(args[0]);
+		Scanner reader = new Scanner(input);
+		
+		while(reader.hasNextLine()) {
+			String frequency = reader.nextLine();
+			//System.out.println("frequency = " + frequency);
+			
+		}
+		
+	}
+	
+}
