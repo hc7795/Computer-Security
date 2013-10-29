aab49c26092a18f8bff54916b53a84cbab63fbc4
hc7795 <hc7795@cs.utexas.edu>
2013-10-29 18:06:01 -0500

    Update AES.java

--- AES/AES.java
+++ AES/AES.java
@@ -1,11 +1,11 @@
 import java.io.*;
 import java.util.*;
-
+import java.lang.Integer;
 public class AES {
 	
 	public static String[][] keyArray = new String[4][4];
 	
-	String[][] s ={{"63", "7C", "77", "7B", "F2", "6B", "6F", "C5", "30", "01", "67", "2B", "FE", "D7", "AB", "76"},
+	public static String[][] sbox ={{"63", "7C", "77", "7B", "F2", "6B", "6F", "C5", "30", "01", "67", "2B", "FE", "D7", "AB", "76"},
 		  {"CA", "82", "C9", "7D", "FA", "59", "47", "F0", "AD", "D4", "A2", "AF", "9C", "A4", "72", "C0"},
 		  {"B7", "FD", "93", "26", "36", "3F", "F7", "CC", "34", "A5", "E5", "F1", "71", "D8", "31", "15"},
 		  {"04", "C7", "23", "C3", "18", "96", "05", "9A", "07", "12", "80", "E2", "EB", "27", "B2", "75"},
@@ -23,28 +23,55 @@ public class AES {
 		  {"8C", "A1", "89", "0D", "BF", "E6", "42", "68", "41", "99", "2D", "0F", "B0", "54", "BB", "16"}};
 	
 	public static void main(String[] args) throws IOException{
-		File input = new File(args[1]);
-		Scanner reader = new Scanner(input);
+		File keyFile = new File(args[1]);
+		File inputFile = new File(args[2]);
+		
+		Scanner reader = new Scanner(keyFile);
+		Scanner inputReader = new Scanner(inputFile);
         
-		while(reader.hasNext()) {
-			String inputKey = reader.nextLine();
+		while(inputReader.hasNext()) {
+			String inputLine = inputReader.nextLine();
 			
-			String key = "";
+			String hex = "";
 			int row = 0;
 			int col = 0;
 			int count = 0;//increments a row at every 4 runs
-			for(int i=0; i<inputKey.length()-1; i+=2) {
-				key = inputKey.substring(i, i+2);
+			for(int i=0; i<inputLine.length()-1; i+=2) {
+				hex = inputLine.substring(i, i+2);
 				if(count == 4) {
 					++row;
 					col = 0;
 					count = 0;
 				}
-				keyArray[row][col] = key;
+				keyArray[row][col] = hex;
 				++count;
 				++col;
 			}
+			for (String[] arr : keyArray) {
+	            System.out.println(Arrays.toString(arr));
+	        }
 			
+			System.out.println();
+			subBytes();
+			
+			for (String[] arr : keyArray) {
+	            System.out.println(Arrays.toString(arr));
+	        }
 		}
 	}
+	
+	public static void subBytes() {
+		String hex = "";
+		int row = 0;
+		int col = 0;
+		for(int i = 0; i<keyArray.length; i++) {
+			for(int j = 0; j<keyArray[i].length; j++) {
+				hex = keyArray[i][j];
+				row = Integer.parseInt(Character.toString(hex.charAt(0)), 16);
+				col = Integer.parseInt(Character.toString(hex.charAt(1)), 16);
+				keyArray[i][j] = sbox[row][col];
+			}
+		} 
+	}
+	
 }
